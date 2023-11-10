0e9adf8f7fba3d4fb6e31204ffc69377f4213f4a
Matvii Ilchenko <matvey5i@ukr.net>
2023-11-10 11:26:34 +0200

    all latest updates

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,2 +1,9 @@
-var splide = new Splide( '.splide' );
-splide.mount();
+var splide = new Splide( '.splide', {
+    perPage : 1,
+    lazyLoad: 'nearby',
+    breakpoints: {
+      height: '6rem',
+    },
+  } );
+  
+  splide.mount();
